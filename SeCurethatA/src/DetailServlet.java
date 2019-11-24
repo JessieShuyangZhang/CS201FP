eec8e98a7c45492aaa2519331c6e15dca0298d4d
Ricardo Lee <Ricardolee@IrenedeMacBook-Pro.local>
2019-11-23 16:49:17 -0800

    DetailPage

---
+++ SeCurethatA/src/DetailServlet.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DetailServlet
+ */
+@WebServlet("/DetailServlet")
+public class DetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DetailServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String courseName = request.getParameter("courseName");
+		Database db = new Database();
+		session.setAttribute("courseName", courseName);
+		session.setAttribute("avgGPA", db.getGPA(courseName));
+		session.setAttribute("courseDescription", db.getDescription(courseName));
+	}
+
+}
